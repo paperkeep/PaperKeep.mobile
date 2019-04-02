23e6854bbc620217643b0d8c6b8aa1fabbcf0122
JF <jf@MacBook-Pro-de-JF.local>
2019-04-02 18:39:40 -0400

    chore(*): init project

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">PaperKeep</text>
+  </view>
+  <view class="container">
+    <flat-list
+      :data="[{key: 'a'}, {key: 'b'}, {key: 'c'}]"
+      :render-item="(item) => renderList(item)"
+    />
+  </view>
+</template>
+ 
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
+
+<script>
+import React from 'react';
+import {Text} from 'react-native';
+    export default {
+        methods: {
+            renderList: function(item) {
+                return (<Text>{item.item.key}</Text>)
+            }
+        }
+    }
+</script>
